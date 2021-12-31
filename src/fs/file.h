0dc447676c487bb74a9e1919373ec925454ea711
Tan-YiFan <15711657500@163.com>
2021-12-31 15:06:30 +0800

    Add file layer comment

--- src/fs/file.h
+++ src/fs/file.h
@@ -19,13 +19,43 @@ typedef struct file {
     usize off;
 } File;
 
-
+/* Init File Table Lock. */
 void fileinit();
+
+/* 
+ * Iterate the file table to get a file structure with ref == 0.
+ * Set the ref to 1.
+ */
 struct file *filealloc();
+
+/*
+ * f->ref++
+ * ref is to prevent the file from being closed while it is being holded by others
+ */
 struct file *filedup(struct file *f);
+
+/* 
+ * f->ref--
+ * if (f->ref == 0) close the file and the inode
+ */
 void fileclose(struct file *f);
+
+/* 
+ * Call stati
+ */
 int filestat(struct file *f, struct stat *st);
+
+/* 
+ * Call inodes.read with range [f->off, f->off + n)
+ * Increment f->off by the return value.
+ */
 isize fileread(struct file *f, char *addr, isize n);
+
+/* 
+ * Call inodes.write with range [f->off, f->off + n)
+ * Increment f->off by the return value.
+ * There should be a maximum valid value of n.
+ */
 isize filewrite(struct file *f, char *addr, isize n);
 
 int sys_dup();
